769c2b41ee8752ed4c90d50e4585170d12a33419
angelo morales <angelotuba3@gmail.com>
2024-05-07 17:27:31 +0200

    udate testing users

--- src/app/core/services/users.state.service.spec.ts
+++ src/app/core/services/users.state.service.spec.ts
@@ -1,19 +1,64 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UsersStateService } from './users.state.service';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RepoUsersService } from './repo.users.service';
+import { Observable } from 'rxjs';
 
-describe('UsersStateService', () => {
-  let service: UsersStateService;
+describe('StateService', () => {
+  let stateService: UsersStateService;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let repoUsersService: jasmine.SpyObj<RepoUsersService>;
 
   beforeEach(() => {
+    const repoSpy = jasmine.createSpyObj('RepoUsersService', [
+      'login',
+      'getById',
+      'create',
+    ]);
+
     TestBed.configureTestingModule({
-      providers: [provideHttpClient(withFetch())],
+      imports: [HttpClientTestingModule],
+      providers: [
+        RepoUsersService,
+        { provide: RepoUsersService, useValue: repoSpy },
+      ],
     });
-    service = TestBed.inject(UsersStateService);
+
+    stateService = TestBed.inject(UsersStateService);
+    repoUsersService = TestBed.inject(
+      RepoUsersService
+    ) as jasmine.SpyObj<RepoUsersService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(stateService).toBeTruthy();
+  });
+
+  it('should call setLogin with stored token from localStorage', () => {
+    // Setup
+    const storedToken = 'mockStoredToken';
+    localStorage.setItem('TFD', storedToken);
+    const setLoginSpy = spyOn(stateService, 'setLogin').and.callThrough();
+
+    stateService.setLogin(storedToken);
+
+    expect(setLoginSpy).toHaveBeenCalledWith(storedToken);
+  });
+
+  it('should set logout', () => {
+    spyOn(localStorage, 'removeItem');
+
+    stateService.setLogout();
+
+    expect(localStorage.removeItem).toHaveBeenCalledWith('TFD');
+    expect(stateService.state.loginState).toEqual('idle');
+    expect(stateService.state.token).toBeNull();
+    expect(stateService.state.currentPayload).toBeNull();
+    expect(stateService.state.currentUser).toBeNull();
+  });
+
+  it('should return user state observable', () => {
+    const userState$ = stateService.getState();
+    expect(userState$).toEqual(jasmine.any(Observable));
   });
 });
