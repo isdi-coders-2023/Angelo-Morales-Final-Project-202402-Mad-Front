5524e924b1ac3e8b432aaa4ae2d3b1490e095588
angelo morales <angelotuba3@gmail.com>
2024-05-20 14:44:20 +0200

    update a anclaje

--- src/app/features/shared/header/header.component.ts
+++ src/app/features/shared/header/header.component.ts
@@ -7,40 +7,37 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   template: `<header>
     <div class="header-icons">
-      <a href="">
-        <img
-          src="../../../../assets/lupaicon.jpeg"
-          alt="icono para buscar"
-          width="16"
-          height="16"
-        />
-      </a>
+      <img
+        src="../../../../assets/lupaicon.jpeg"
+        alt="icono para buscar"
+        width="16"
+        height="16"
+      />
 
-      <a [routerLink]="['/login']" routerLinkActive="active">
-        <img
-          src="../../../../assets/cuenta-de-usuario.png"
-          alt="icon para ir a login"
-          width="21"
-          height="21"
-        />
-      </a>
-      <a>
-        <img
-          [routerLink]="['/home']"
-          routerLinkActive="active"
-          src="../../../../assets/angelologo.png"
-          alt="logo angelo"
-          width="201"
-        />
-      </a>
-      <a href="">
-        <img
-          src="../../../../assets/bolsa compra.jpeg"
-          alt="bolsa de compra"
-          width="17"
-          height="17"
-        />
-      </a>
+      <img
+        [routerLink]="['/login']"
+        routerLinkActive="active"
+        src="../../../../assets/cuenta-de-usuario.png"
+        alt="icon para ir a login"
+        width="21"
+        height="21"
+      />
+
+      <img
+        [routerLink]="['/home']"
+        routerLinkActive="active"
+        src="../../../../assets/angelologo.png"
+        alt="logo angelo"
+        width="201"
+      />
+
+      <img
+        src="../../../../assets/bolsa compra.jpeg"
+        alt="bolsa de compra"
+        width="17"
+        height="17"
+      />
+
       <div class="header">
         <button class="menu-hamburguesa" (click)="toggleMenu()">
           <img
